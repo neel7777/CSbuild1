997fd48d8bd63aa31621ed0d1b83fe4622cd03b7
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-07-22 22:17:30 -0400

    made a grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { AgGridReact } from 'ag-grid-react';
+
 import './App.css';
 
 function App() {
+
+  const CELL_SIZE = 20; 
+  const WIDTH = 800;
+  const HEIGHT = 600;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conway's Game of Life</h1>
+      <div className="Board"          style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>        
+      </div>
+      
+     
+      
+      
     </div>
   );
 }
